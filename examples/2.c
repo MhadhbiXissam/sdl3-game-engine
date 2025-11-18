7590ff2be4679668fde36879fa1ffce8e26abcae
$GITLAB_USER_LOGIN <$GITLAB_USER_EMAIL>
2025-11-18 22:55:04 +0100

    examples-good

---
+++ examples/2.c
@@ -0,0 +1,273 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL_opengles2.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+#define MAX_BLOCKS 5
+
+/*------------------- Engine Structs -------------------*/
+typedef struct { float x, y, z, w; } Vec4f;
+typedef struct Engine Engine;
+struct Engine {
+    SDL_Window* window;
+    SDL_GLContext context;
+    Uint32 Windowflags;
+    int width, height;
+    SDL_Event event;
+    int run;
+    void (*eventHandler)(Engine*);
+    void (*draw)(Engine*);
+    void (*on_engine_started)(Engine*);
+    void (*on_engine_closed)(Engine*);
+    void (*on_engine_swaped)(Engine*);
+    Vec4f clearcolor;
+};
+
+/*------------------- Engine Functions -------------------*/
+bool engine_start(Engine* engine){
+    SDL_zero(engine->event);
+    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0){
+        SDL_Log("SDL_Init failed: %s", SDL_GetError());
+        return false;
+    }
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    if(!engine->Windowflags) engine->Windowflags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL;
+    if(!engine->width) engine->width = 360;
+    if(!engine->height) engine->height = 600;
+
+    engine->window = SDL_CreateWindow("SDL3 OpenGL ES 2D Playground",
+                                      engine->width, engine->height,
+                                      engine->Windowflags);
+    if(!engine->window){
+        SDL_Log("SDL_CreateWindow failed: %s", SDL_GetError());
+        SDL_Quit();
+        return false;
+    }
+
+    engine->context = SDL_GL_CreateContext(engine->window);
+    if(!engine->context){
+        SDL_Log("SDL_GL_CreateContext failed: %s", SDL_GetError());
+        SDL_DestroyWindow(engine->window);
+        SDL_Quit();
+        return false;
+    }
+
+    if(SDL_GL_MakeCurrent(engine->window, engine->context) < 0){
+        SDL_Log("SDL_GL_MakeCurrent failed: %s", SDL_GetError());
+        SDL_GL_DestroyContext(engine->context);
+        SDL_DestroyWindow(engine->window);
+        SDL_Quit();
+        return false;
+    }
+
+    glViewport(0, 0, engine->width, engine->height);
+    engine->run = 1;
+    return true;
+}
+
+void engine_destroy(Engine* engine){
+    if(engine->context) SDL_GL_DestroyContext(engine->context);
+    if(engine->window) SDL_DestroyWindow(engine->window);
+    SDL_Quit();
+}
+
+void engine_getevent(Engine* engine){
+    SDL_Event event;
+    while(SDL_PollEvent(&event)){
+        engine->event = event;
+        if(event.type == SDL_EVENT_QUIT) engine->run = 0;
+        if(event.type == SDL_EVENT_WINDOW_RESIZED){
+            int w = event.window.data1;
+            int h = event.window.data2;
+            glViewport(0, 0, w, h);
+        }
+    }
+}
+
+void engine_swap(Engine* engine){ SDL_GL_SwapWindow(engine->window); }
+int engine_run(Engine* engine){
+    if(!engine_start(engine)) return 1;
+    if(engine->on_engine_started) engine->on_engine_started(engine);
+    while(engine->run){
+        engine_getevent(engine);
+        if(engine->eventHandler) engine->eventHandler(engine);
+
+        glClearColor(engine->clearcolor.x, engine->clearcolor.y, engine->clearcolor.z, engine->clearcolor.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        if(engine->draw) engine->draw(engine);
+
+        if(engine->on_engine_swaped) engine->on_engine_swaped(engine);
+
+        engine_swap(engine);
+    }
+    if(engine->on_engine_closed) engine->on_engine_closed(engine);
+    engine_destroy(engine);
+    return 0;
+}
+
+/*------------------- Game Code -------------------*/
+typedef struct {
+    float x, y, w, h;
+    float speedX, speedY;
+} Block;
+
+Block blocks[MAX_BLOCKS];
+Block player;
+float touchX=-1, touchY=-1;
+float timeValue=0.0f;
+
+GLuint shaderProgram, vbo;
+
+const char* vertexShaderSrc =
+"attribute vec2 aPos;\n"
+"void main(){\n"
+"   gl_Position = vec4(aPos, 0.0, 1.0);\n"
+"}\n";
+
+const char* fragmentShaderSrc =
+"precision mediump float;\n"
+"uniform float uTime;\n"
+"uniform vec2 uResolution;\n"
+"void main(){\n"
+"   vec2 st = gl_FragCoord.xy / uResolution;\n"
+"   float r = 0.5 + 0.5*sin(uTime + st.x*10.0);\n"
+"   float g = 0.5 + 0.5*sin(uTime + st.y*10.0);\n"
+"   float b = 0.5 + 0.5*sin(uTime);\n"
+"   gl_FragColor = vec4(r, g, b, 1.0);\n"
+"}\n";
+
+GLuint compileShader(GLenum type, const char* source){
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader,1,&source,NULL);
+    glCompileShader(shader);
+    GLint success; glGetShaderiv(shader, GL_COMPILE_STATUS,&success);
+    if(!success){ char info[512]; glGetShaderInfoLog(shader,512,NULL,info); printf("Shader compile error: %s\n", info); }
+    return shader;
+}
+
+void setupShaders(){
+    GLuint vert = compileShader(GL_VERTEX_SHADER,vertexShaderSrc);
+    GLuint frag = compileShader(GL_FRAGMENT_SHADER,fragmentShaderSrc);
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vert);
+    glAttachShader(shaderProgram, frag);
+    glLinkProgram(shaderProgram);
+    glDeleteShader(vert); glDeleteShader(frag);
+}
+
+void setupBlocks(int width,int height){
+    for(int i=0;i<MAX_BLOCKS;i++){
+        blocks[i].w = 50; blocks[i].h=50;
+        blocks[i].x = rand() % (width - 50);
+        blocks[i].y = rand() % (height - 50);
+        blocks[i].speedX = (rand()%3 +1)*((rand()%2)?1:-1);
+        blocks[i].speedY = (rand()%3 +1)*((rand()%2)?1:-1);
+    }
+}
+
+float pxToNDCx(float x,int width){ return (x/(width/2))-1.0f; }
+float pxToNDCy(float y,int height){ return 1.0f-(y/(height/2)); }
+
+void engine_eventHandler(Engine* engine){
+    const Uint8* state = SDL_GetKeyboardState(NULL);
+    if(state[SDL_SCANCODE_UP]) player.y -= player.speedY;
+    if(state[SDL_SCANCODE_DOWN]) player.y += player.speedY;
+    if(state[SDL_SCANCODE_LEFT]) player.x -= player.speedX;
+    if(state[SDL_SCANCODE_RIGHT]) player.x += player.speedX;
+
+    SDL_Event e = engine->event;
+    if(e.type==SDL_EVENT_FINGER_DOWN || e.type==SDL_EVENT_FINGER_MOTION){
+        touchX = e.tfinger.x*engine->width;
+        touchY = e.tfinger.y*engine->height;
+        player.x = touchX - player.w/2;
+        player.y = touchY - player.h/2;
+    }
+}
+
+void engine_started(Engine* engine){
+    player.x = engine->width/2-25;
+    player.y = engine->height/2-25;
+    player.w = 50; player.h=50;
+    player.speedX=5; player.speedY=5;
+
+    setupShaders();
+    setupBlocks(engine->width,engine->height);
+
+    float vertices[] = { -0.1f,-0.1f, 0.1f,-0.1f, 0.1f,0.1f, -0.1f,0.1f };
+    glGenBuffers(1,&vbo);
+    glBindBuffer(GL_ARRAY_BUFFER,vbo);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_DYNAMIC_DRAW);
+}
+
+void updateBlocks(Engine* engine){
+    for(int i=0;i<MAX_BLOCKS;i++){
+        blocks[i].x += blocks[i].speedX;
+        blocks[i].y += blocks[i].speedY;
+
+        if(blocks[i].x<0 || blocks[i].x+blocks[i].w>engine->width) blocks[i].speedX*=-1;
+        if(blocks[i].y<0 || blocks[i].y+blocks[i].h>engine->height) blocks[i].speedY*=-1;
+
+        // Simple collision with player
+        if(player.x < blocks[i].x+blocks[i].w && player.x+player.w>blocks[i].x &&
+           player.y < blocks[i].y+blocks[i].h && player.y+player.h>blocks[i].y){
+            blocks[i].speedX*=-1;
+            blocks[i].speedY*=-1;
+        }
+    }
+}
+
+void engine_draw(Engine* engine){
+    timeValue += 0.05f;
+
+    glUseProgram(shaderProgram);
+
+    GLint posLoc = glGetAttribLocation(shaderProgram,"aPos");
+    GLint timeLoc = glGetUniformLocation(shaderProgram,"uTime");
+    GLint resLoc  = glGetUniformLocation(shaderProgram,"uResolution");
+    glUniform1f(timeLoc,timeValue);
+    glUniform2f(resLoc,(float)engine->width,(float)engine->height);
+
+    updateBlocks(engine);
+
+    Block all[MAX_BLOCKS+1];
+    all[0]=player;
+    for(int i=0;i<MAX_BLOCKS;i++) all[i+1]=blocks[i];
+
+    for(int i=0;i<=MAX_BLOCKS;i++){
+        float x1=pxToNDCx(all[i].x,engine->width);
+        float y1=pxToNDCy(all[i].y,engine->height);
+        float x2=pxToNDCx(all[i].x+all[i].w,engine->width);
+        float y2=pxToNDCy(all[i].y+all[i].h,engine->height);
+        float vertices[] = { x1,y1, x2,y1, x2,y2, x1,y2 };
+        glBindBuffer(GL_ARRAY_BUFFER,vbo);
+        glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_DYNAMIC_DRAW);
+        glEnableVertexAttribArray(posLoc);
+        glVertexAttribPointer(posLoc,2,GL_FLOAT,GL_FALSE,0,0);
+        glDrawArrays(GL_TRIANGLE_FAN,0,4);
+    }
+}
+
+void on_engine_closed(Engine* engine){ printf("Engine closed\n"); }
+void on_engine_swaped(Engine* engine){}
+
+int main(int argc,char* argv[]){
+    Engine engine; SDL_zero(engine);
+    engine.Windowflags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL;
+    engine.width=360; engine.height=600;
+    engine.eventHandler=engine_eventHandler;
+    engine.on_engine_started=engine_started;
+    engine.draw=engine_draw;
+    engine.on_engine_closed=on_engine_closed;
+    engine.on_engine_swaped=on_engine_swaped;
+    engine.clearcolor.x=0.1f; engine.clearcolor.y=0.1f;
+    engine.clearcolor.z=0.1f; engine.clearcolor.w=1.0f;
+
+    return engine_run(&engine);
+}
---
+++ examples/4.c
@@ -0,0 +1,290 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL_opengles2.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+
+#define MAX_NODES 10
+#define DEG2RAD(a) ((a) * 0.0174533f)
+
+/*---------------- Node & Scene ----------------*/
+typedef struct Node Node;
+typedef struct Scene Scene;
+
+struct Node {
+    float x, y;       // position
+    float rotation;   // in degrees
+    float scaleX, scaleY;
+    int renderable;
+    int rotate;       // auto rotate
+    float rotation_speed;
+    Node* children[MAX_NODES];
+    int child_count;
+};
+
+struct Scene {
+    Node* root;
+    float rotation;
+};
+
+/*---------------- Game & Engine ----------------*/
+typedef struct {
+    int state;
+    Scene** scenes;
+    int scene_count;
+    int current_scene_index;
+} Game;
+
+typedef struct Engine Engine;
+struct Engine {
+    SDL_Window* window;
+    SDL_GLContext context;
+    Uint32 Windowflags;
+    int width, height;
+    SDL_Event event;
+    int run;
+    void (*eventHandler)(Engine*);
+    void (*draw)(Engine*);
+    void (*on_engine_started)(Engine*);
+    void (*on_engine_closed)(Engine*);
+    void (*on_engine_swaped)(Engine*);
+    Game* game;
+    float clearcolor[4];
+    GLuint shaderProgram;
+    GLuint vbo;
+};
+
+/*---------------- Matrix Helper ----------------*/
+void mat4_identity(float* m){
+    for(int i=0;i<16;i++) m[i]=0.0f;
+    m[0]=1; m[5]=1; m[10]=1; m[15]=1;
+}
+
+void mat4_translate(float* m, float x, float y, float z){
+    m[12] += x;
+    m[13] += y;
+    m[14] += z;
+}
+
+void mat4_scale(float* m, float sx, float sy, float sz){
+    m[0]*=sx; m[5]*=sy; m[10]*=sz;
+}
+
+void mat4_rotate_z(float* m, float angleDeg){
+    float rad = angleDeg * 0.0174533f;
+    float c=cosf(rad), s=sinf(rad);
+    float m0=m[0], m4=m[4], m1=m[1], m5=m[5];
+    m[0]=c*m0+s*m1; m[1]=-s*m0+c*m1;
+    m[4]=c*m4+s*m5; m[5]=-s*m4+c*m5;
+}
+
+/*---------------- Shader ----------------*/
+const char* vertexShaderSrc =
+"attribute vec2 aPos;\n"
+"uniform mat4 uModel;\n"
+"void main(){\n"
+"   gl_Position = uModel * vec4(aPos,0.0,1.0);\n"
+"}\n";
+
+const char* fragmentShaderSrc =
+"precision mediump float;\n"
+"uniform vec3 uColor;\n"
+"void main(){ gl_FragColor = vec4(uColor,1.0); }\n";
+
+GLuint compileShader(GLenum type,const char* src){
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader,1,&src,NULL);
+    glCompileShader(shader);
+    GLint success; glGetShaderiv(shader,GL_COMPILE_STATUS,&success);
+    if(!success){ char info[512]; glGetShaderInfoLog(shader,512,NULL,info); printf("Shader error: %s\n",info);}
+    return shader;
+}
+
+void setupShaders(Engine* engine){
+    GLuint vert = compileShader(GL_VERTEX_SHADER,vertexShaderSrc);
+    GLuint frag = compileShader(GL_FRAGMENT_SHADER,fragmentShaderSrc);
+    engine->shaderProgram = glCreateProgram();
+    glAttachShader(engine->shaderProgram,vert);
+    glAttachShader(engine->shaderProgram,frag);
+    glLinkProgram(engine->shaderProgram);
+    glDeleteShader(vert); glDeleteShader(frag);
+
+    float vertices[]={-0.05f,-0.05f, 0.05f,-0.05f, 0.05f,0.05f, -0.05f,0.05f};
+    glGenBuffers(1,&engine->vbo);
+    glBindBuffer(GL_ARRAY_BUFFER,engine->vbo);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+}
+
+/*---------------- Node Tree ----------------*/
+Node* create_node(float x,float y,int renderable,int rotate,float speed,float sx,float sy){
+    Node* n = (Node*)malloc(sizeof(Node));
+    n->x=x; n->y=y; n->rotation=0;
+    n->scaleX=sx; n->scaleY=sy;
+    n->renderable=renderable; n->rotate=rotate; n->rotation_speed=speed;
+    n->child_count=0;
+    for(int i=0;i<MAX_NODES;i++) n->children[i]=NULL;
+    return n;
+}
+
+void add_child(Node* parent, Node* child){
+    if(parent->child_count<MAX_NODES) parent->children[parent->child_count++] = child;
+}
+
+void animate_node(Node* node){
+    if(!node) return;
+    if(node->rotate) node->rotation += node->rotation_speed;
+    for(int i=0;i<node->child_count;i++) animate_node(node->children[i]);
+}
+
+void render_node(Engine* engine, Node* node, float* parentMat){
+    if(!node) return;
+    float model[16];
+    for(int i=0;i<16;i++) model[i]=parentMat[i];
+
+    mat4_translate(model,node->x,node->y,0);
+    mat4_rotate_z(model,node->rotation);
+    mat4_scale(model,node->scaleX,node->scaleY,1.0f);
+
+    GLuint modelLoc = glGetUniformLocation(engine->shaderProgram,"uModel");
+    glUniformMatrix4fv(modelLoc,1,GL_FALSE,model);
+
+    if(node->renderable){
+        GLuint posLoc = glGetAttribLocation(engine->shaderProgram,"aPos");
+        glEnableVertexAttribArray(posLoc);
+        glBindBuffer(GL_ARRAY_BUFFER,engine->vbo);
+        glVertexAttribPointer(posLoc,2,GL_FLOAT,GL_FALSE,0,0);
+        GLuint colorLoc = glGetUniformLocation(engine->shaderProgram,"uColor");
+        glUniform3f(colorLoc,1.0f,0.0f,0.0f);
+        glDrawArrays(GL_TRIANGLE_FAN,0,4);
+    }
+
+    for(int i=0;i<node->child_count;i++) render_node(engine,node->children[i],model);
+}
+
+void render_scene(Engine* engine, Scene* scene){
+    float model[16]; mat4_identity(model);
+    mat4_rotate_z(model,scene->rotation);
+    animate_node(scene->root);
+    render_node(engine,scene->root,model);
+}
+
+/*---------------- Game ----------------*/
+Game* create_game(){
+    Game* g = (Game*)malloc(sizeof(Game));
+    g->state=0; g->scenes=NULL; g->scene_count=0; g->current_scene_index=0;
+    return g;
+}
+
+void add_scene(Game* g, Scene* s){
+    g->scenes = (Scene**)realloc(g->scenes,sizeof(Scene*)*(g->scene_count+1));
+    g->scenes[g->scene_count++] = s;
+}
+
+Scene* get_current_scene(Game* g){
+    if(g->scene_count==0) return NULL;
+    return g->scenes[g->current_scene_index];
+}
+
+/*---------------- Engine Callbacks ----------------*/
+void engine_eventHandler(Engine* engine){
+    const Uint8* state = SDL_GetKeyboardState(NULL);
+    Scene* s = get_current_scene(engine->game);
+    if(!s) return;
+    if(state[SDL_SCANCODE_LEFT]) s->rotation-=1.0f;
+    if(state[SDL_SCANCODE_RIGHT]) s->rotation+=1.0f;
+
+    // trigger rotation for individual nodes
+    if(state[SDL_SCANCODE_A]) s->root->rotate=1;
+    if(state[SDL_SCANCODE_S] && s->root->child_count>0) s->root->children[0]->rotate=1;
+    if(state[SDL_SCANCODE_D] && s->root->child_count>1) s->root->children[1]->rotate=1;
+}
+
+void engine_draw(Engine* engine){
+    Scene* s = get_current_scene(engine->game);
+    glUseProgram(engine->shaderProgram);
+    if(s) render_scene(engine,s);
+}
+
+void engine_started(Engine* engine){
+    setupShaders(engine);
+    Node* root = create_node(0.0f,0.0f,0,0,0,1.0f,1.0f);
+    Node* c1 = create_node(0.3f,0.0f,1,0,2.0f,0.5f,0.5f);
+    Node* c2 = create_node(-0.3f,0.0f,1,0,1.5f,0.5f,0.5f);
+    Node* gc = create_node(0.0f,0.2f,1,0,3.0f,0.3f,0.3f);
+    add_child(c1,gc);
+    add_child(root,c1);
+    add_child(root,c2);
+    Scene* scene = (Scene*)malloc(sizeof(Scene));
+    scene->root=root; scene->rotation=0;
+    add_scene(engine->game,scene);
+    engine->clearcolor[0]=0.1f; engine->clearcolor[1]=0.1f; engine->clearcolor[2]=0.1f; engine->clearcolor[3]=1.0f;
+}
+
+void on_engine_closed(Engine* engine){ printf("Engine closed\n"); }
+void on_engine_swaped(Engine* engine){}
+
+/*---------------- Engine Core ----------------*/
+bool engine_start(Engine* engine){
+    if(SDL_Init(SDL_INIT_VIDEO|SDL_INIT_EVENTS)<0){ printf("SDL_Init failed: %s\n",SDL_GetError()); return false; }
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION,2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION,0);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,1);
+    engine->window = SDL_CreateWindow("GLES2 Scene",engine->width,engine->height,engine->Windowflags);
+    if(!engine->window){ printf("SDL_CreateWindow failed\n"); SDL_Quit(); return false; }
+    engine->context = SDL_GL_CreateContext(engine->window);
+    if(!engine->context){ printf("SDL_GL_CreateContext failed\n"); SDL_DestroyWindow(engine->window); SDL_Quit(); return false; }
+    SDL_GL_MakeCurrent(engine->window,engine->context);
+    glViewport(0,0,engine->width,engine->height);
+    engine->run=1;
+    return true;
+}
+
+void engine_destroy(Engine* engine){
+    if(engine->context) SDL_GL_DestroyContext(engine->context);
+    if(engine->window) SDL_DestroyWindow(engine->window);
+    SDL_Quit();
+}
+
+void engine_getevent(Engine* engine){
+    SDL_Event e;
+    while(SDL_PollEvent(&e)){
+        engine->event=e;
+        if(e.type==SDL_EVENT_QUIT) engine->run=0;
+    }
+}
+
+void engine_swap(Engine* engine){ SDL_GL_SwapWindow(engine->window); }
+
+int engine_run(Engine* engine){
+    if(!engine_start(engine)) return 1;
+    if(engine->on_engine_started) engine->on_engine_started(engine);
+    while(engine->run){
+        engine_getevent(engine);
+        if(engine->eventHandler) engine->eventHandler(engine);
+        glClearColor(engine->clearcolor[0],engine->clearcolor[1],engine->clearcolor[2],engine->clearcolor[3]);
+        glClear(GL_COLOR_BUFFER_BIT);
+        if(engine->draw) engine->draw(engine);
+        if(engine->on_engine_swaped) engine->on_engine_swaped(engine);
+        engine_swap(engine);
+    }
+    if(engine->on_engine_closed) engine->on_engine_closed(engine);
+    engine_destroy(engine);
+    return 0;
+}
+
+/*---------------- Main ----------------*/
+int main(int argc,char* argv[]){
+    Engine engine; SDL_zero(engine);
+    engine.Windowflags = SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE;
+    engine.width=800; engine.height=600;
+    engine.eventHandler=engine_eventHandler;
+    engine.draw=engine_draw;
+    engine.on_engine_started=engine_started;
+    engine.on_engine_closed=on_engine_closed;
+    engine.on_engine_swaped=on_engine_swaped;
+    engine.game=create_game();
+    return engine_run(&engine);
+}
