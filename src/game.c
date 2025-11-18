7484e8ba96aa9fdd20a092cd7f49e1a7d1b71958
$GITLAB_USER_LOGIN <$GITLAB_USER_EMAIL>
2025-11-18 01:28:48 +0100

    explained template

--- src/game.c
+++ src/game.c
@@ -1,21 +1,25 @@
+// this struct will be shared accros all methods ( engine->game )
 typedef struct {
     int score ; 
     
 } Game; 
 
+// this methode used to draw opengles stuffs 
 void draw(void* e){
     printf("Called draw function ... \n") ; 
 }
 
+// this methode called once the engine stared ( window and context created )
 void on_engine_started(void* e){
     printf("Called on_engine_started function... \n") ; 
 }
 
+// this method called before engine will close (sdl quit )
 void on_engine_close(void* e){
     printf("Called on_engine_close function... \n") ; 
 }
 
-
+// this methode called before sdl gl swap window 
 void on_engine_swap(void* e){
     Engine* engine = (Engine*)e ; 
     Game* game = (Game*)(engine->game);
@@ -24,6 +28,7 @@ void on_engine_swap(void* e){
 }
 
 
+// this methode is used to handle sdl3 events 
 void eventHandler(void* e){
     Engine* engine = (Engine*)e ; 
     Game* game = (Game*)(engine->game);
