28d56cc301d921566844290a49df1e4aea3caac9
$GITLAB_USER_LOGIN <$GITLAB_USER_EMAIL>
2025-11-19 04:49:10 +0100

    completed ok

--- src/game.c
+++ src/game.c
@@ -1,56 +1,40 @@
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
-#include <SDL3/SDL_opengles2.h> 
 #include <stdio.h>
-
-/*............................................................*/
-typedef struct {
-    float x ; float y ; float z  ; float w ; 
-} Vec4f ; 
-/*............................................................*/
-typedef struct {
-    float x ; float y ; float z  ; 
-} Vec3f ; 
-/*............................................................*/
-typedef struct {
-    float x ; float y ; float z  ; float w ; 
-} Vec4i ; 
-/*............................................................*/
-typedef struct {
-    float x ; float y ; float z  ; 
-} Vec3i ; 
-/*............................................................*/
-/*............................................................*/
-
-typedef enum {
-    ALICE_ID,
-    BOB_ID,
-    CHARLIE_ID
-} SceneNodeType;
-
-typedef struct SceneNode SceneNode; // forward declaration
+#include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct {
     int id ; 
-    Vec3f position ; 
-    Vec3f rotation ; 
-    Vec3f scale ; 
-    SceneNodeType typ ; 
-    SceneNode **childreen ;
-    SceneNode *parent ; 
-} SceneNode ; 
+}GameState ; 
 
-typedef struct {
-    
-} Scene ; 
 
-typedef struct {
-    
-} GameData ; 
 
-typedef struct {
-    GameData* data ; // saved game to saved here , they should loaded from engine infrastructure to here on game started 
 
-} Game ; 
+GameState* on_game_started(){
+    GameState* state =  (GameState*)malloc(sizeof(GameState));
+    printf("game loaded...\n");
+    state->id = 120 ;
+    return state;
 
-void on_game_started(Game* game)
+}
+
+
+
+void on_game_event(GameState* state , int typ ){
+    printf("recieved event id = ...%i.\n",typ); 
+
+}
+
+
+void on_game_update(GameState* state ){
+    state->id += 1 ;
+    printf("game updating state.id...%i.\n",state->id); 
+
+
+}
+
+
+void on_game_finalize(void* state ){
+    free(state) ; 
+    printf("game final "); 
+}
+
--- src/game_engine.c
+++ src/game_engine.c
@@ -1,8 +1,65 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <dlfcn.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_opengles2.h> 
-#include <stdio.h>
+#include <time.h>
+#include <stdbool.h>
+// Macro shortcuts for SDL logging
+#define LOG_LEVEL SDL_LOG_PRIORITY_VERBOSE
+#define printInfo(msg, ...)  SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, msg, ##__VA_ARGS__)
+#define printWarn(msg, ...)  SDL_LogWarn(SDL_LOG_CATEGORY_APPLICATION, msg, ##__VA_ARGS__)
+#define printError(msg, ...) SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, msg, ##__VA_ARGS__)
+#define printDebug(msg, ...) SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, msg, ##__VA_ARGS__)
+#define printVerbose(msg, ...) SDL_LogVerbose(SDL_LOG_CATEGORY_APPLICATION, msg, ##__VA_ARGS__)
+void game_engine_logHandler(void *userdata, int category, SDL_LogPriority priority, const char *message) {
+   // ANSI color codes
+   const char *RESET   = "\x1b[0m";
+   const char *RED     = "\x1b[31m";
+   const char *YELLOW  = "\x1b[33m";
+   const char *GREEN   = "\x1b[32m";
+   const char *BLUE    = "\x1b[34m";
+   const char *MAGENTA = "\x1b[35m";
+   const char *CYAN    = "\x1b[36m";
+   const char *WHITE   = "\x1b[37m";
 
+   // Static state to track last message and repeat count
+   static char last_message[1024] = "";
+   static int repeat_count = 0;
+
+   // Map priority to string and color
+   const char *level;
+   const char *color;
+   switch (priority) {
+       case SDL_LOG_PRIORITY_VERBOSE: level = "VERBOSE"; color = CYAN; break;
+       case SDL_LOG_PRIORITY_DEBUG:   level = "DEBUG";   color = BLUE; break;
+       case SDL_LOG_PRIORITY_INFO:    level = "INFO";    color = GREEN; break;
+       case SDL_LOG_PRIORITY_WARN:    level = "WARN";    color = YELLOW; break;
+       case SDL_LOG_PRIORITY_ERROR:   level = "ERROR";   color = RED; break;
+       case SDL_LOG_PRIORITY_CRITICAL:level = "CRITICAL"; color = MAGENTA; break;
+       default: level = "UNKNOWN"; color = WHITE; break;
+   }
+
+   // Get current time
+   time_t now = time(NULL);
+   struct tm *tm_info = localtime(&now);
+   char time_buf[9]; // HH:MM:SS
+   strftime(time_buf, sizeof(time_buf), "%H:%M:%S", tm_info);
+
+   // Handle repeated messages
+   if (strcmp(message, last_message) == 0) {
+       repeat_count++;
+       printf("\r%s[%s] [%s] %s (x%d)%s", color, time_buf, level, message, repeat_count + 1, RESET);
+       fflush(stdout);
+   } else {
+       if (repeat_count > 0) printf("\n"); // finish previous repeated line
+       repeat_count = 0;
+       strncpy(last_message, message, sizeof(last_message) - 1);
+       last_message[sizeof(last_message) - 1] = '\0';
+       printf("%s[%s] [engine] [%s] %s%s\n", color, time_buf, level, message, RESET);
+   }
+}
 /*............................................................*/
 typedef struct {
     float x ; float y ; float z  ; float w ; 
@@ -22,19 +79,16 @@ typedef struct {
 /*............................................................*/
 /*............................................................*/
 
-typedef struct {
-    
-} Scene ; 
 
-typedef struct {
-    
-} GameData ; 
-
-typedef struct {
-    GameData* data ; 
-} Game ; 
-
-
+typedef struct GameState GameState;
+typedef GameState* (*ON_GAME_STARTED)(void);
+static ON_GAME_STARTED PROC_ON_GAME_STARTED ; 
+typedef void (*ON_GAME_UPDATE)(GameState*); 
+static ON_GAME_UPDATE PROC_ON_GAME_UPDATE ; 
+typedef void (*ON_GAME_EVENT)(GameState*,int); 
+static ON_GAME_EVENT PROC_ON_GAME_EVENT ; 
+typedef void (*ON_GAME_FINALIZE)(GameState*); 
+static ON_GAME_FINALIZE PROC_ON_GAME_FINALIZE ; 
 /*++++++++++++++++++++++++++++++++++++++++++++++*/
 typedef struct Engine Engine; // forward declaration
 struct Engine {
@@ -46,12 +100,15 @@ struct Engine {
     int run ; 
     void (*eventHandler)(Engine*); /* Engine* */
     void (*draw)(Engine*); /* Engine* */
-    void (*on_engine_started)(Engine*) ; // call this method when window started 
+    void  (*on_engine_started)(Engine*) ; // call this method when window started 
     void (*on_engine_closed)(Engine*) ;  // call this method on engine before it closes 
     void (*on_engine_swaped)(Engine*) ;  // call this method before swap
     Vec4f clearcolor ; 
-    Game* game ; 
+    void* gameloader ; 
+    GameState* gamestate ; 
 } ;
+
+
 /*++++++++++++++++++++++++++++++++++++++++++++++*/
 bool engine_start(Engine* engine);
 /*++++++++++++++++++++++++++++++++++++++++++++++*/
@@ -65,7 +122,7 @@ int  engine_run(Engine* engine) ;
 /*++++++++++++++++++++++++++++++++++++++++++++++*/
 void engine_eventHandler(Engine* engine ); 
 /*++++++++++++++++++++++++++++++++++++++++++++++*/
-void engine_started(Engine* engine ); 
+int engine_started(Engine* engine ); 
 /*++++++++++++++++++++++++++++++++++++++++++++++*/
 void engine_draw(Engine* engine ); 
 /*++++++++++++++++++++++++++++++++++++++++++++++*/
@@ -73,12 +130,14 @@ void on_engine_closed(Engine* engine );
 /*++++++++++++++++++++++++++++++++++++++++++++++*/
 void on_engine_swaped(Engine* engine ); 
 /*++++++++++++++++++++++++++++++++++++++++++++++*/
+
 /*++++++++++++++++++++++++++++++++++++++++++++++*/
 /*++++++++++++++++++++++++++++++++++++++++++++++*/
 
 /*----------------------------------------------------------------------------------------------------------------------------------------------------*/
 int main(int argc, char *argv[]){
     Engine engine;SDL_zero(engine);
+    SDL_SetLogOutputFunction(game_engine_logHandler, NULL);SDL_SetLogPriorities(LOG_LEVEL);
     engine.Windowflags =  SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL;
     engine.width = 392 ; engine.height = 735 ; 
     engine.eventHandler = engine_eventHandler ; 
@@ -95,7 +154,7 @@ bool engine_start(Engine* engine){
     SDL_zero(engine->event);
     // Initialize SDL with error checking
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        SDL_Log("SDL_Init failed: %s", SDL_GetError());
+        printError("SDL_Init failed: %s", SDL_GetError());
         return false;
     }
     // Set GL attributes
@@ -117,14 +176,14 @@ bool engine_start(Engine* engine){
                                       engine->width, engine->height,
                                       engine->Windowflags);
     if (!engine->window) {
-        SDL_Log("SDL_CreateWindow failed: %s", SDL_GetError());
+        printError("SDL_CreateWindow failed: %s", SDL_GetError());
         SDL_Quit();
         return false;
     }
     // Create GL context
     engine->context = SDL_GL_CreateContext(engine->window);
     if (!engine->context) {
-        SDL_Log("SDL_GL_CreateContext failed: %s", SDL_GetError());
+        printError("SDL_GL_CreateContext failed: %s", SDL_GetError());
         SDL_DestroyWindow(engine->window);
         SDL_Quit();
         return false;
@@ -194,7 +253,6 @@ int engine_run(Engine* engine) {
             engine->eventHandler(engine) ; 
         }
         glClearColor(engine->clearcolor.x, engine->clearcolor.y, engine->clearcolor.z, engine->clearcolor.w);
-        //printf("engine clear color %f ,%f , %f , %f  ",engine->clearcolor.x, engine->clearcolor.y, engine->clearcolor.z, engine->clearcolor.w);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         if (has_draw) {
             engine->draw(engine) ; 
@@ -215,23 +273,53 @@ int engine_run(Engine* engine) {
 // hanndle captured event here 
 void engine_eventHandler(Engine* engine ){
     SDL_Event e = engine->event ; 
-    //printf("[info]:engine_eventHandler ..\n") ; 
+    // process events here 
+    PROC_ON_GAME_EVENT(engine->gamestate,e.type) ; 
 }
 //############################################
-void engine_started(Engine* engine ){
-    printf("engine_started ..") ; 
+int  engine_started(Engine* engine ){
+    printInfo("engine_started ..") ; 
+    engine->gameloader = dlopen("src/libgame.so", RTLD_NOW);
+    if (!(engine->gameloader)) {
+        printError("%s\n", dlerror());
+        dlclose(engine->gameloader);
+        return -1 ; 
+    }
+    engine->gamestate = NULL ;
+
+    PROC_ON_GAME_STARTED =  (ON_GAME_STARTED)dlsym(engine->gameloader, "on_game_started") ; 
+    if(!PROC_ON_GAME_STARTED){printError("%s\n", dlerror());}else{
+        engine->gamestate = PROC_ON_GAME_STARTED() ;
+
+    }
+    PROC_ON_GAME_UPDATE = (ON_GAME_UPDATE)dlsym(engine->gameloader, "on_game_update") ; 
+    if(!PROC_ON_GAME_UPDATE){printError("%s\n", dlerror());}
+    PROC_ON_GAME_FINALIZE = (ON_GAME_FINALIZE)dlsym(engine->gameloader, "on_game_finalize") ; 
+    if(!PROC_ON_GAME_FINALIZE){printError("%s\n", dlerror());}
+    PROC_ON_GAME_EVENT = (ON_GAME_EVENT)dlsym(engine->gameloader, "on_game_event") ; 
+    if(!PROC_ON_GAME_EVENT){printError("%s\n", dlerror());}
+    printDebug("here--------------");
+    return 0 ; 
 }
 //############################################
 void engine_draw(Engine* engine ){
-    printf("engine_draw loop  ..\n") ; 
+    printInfo("engine_draw loop  ..\n") ; 
+    if(PROC_ON_GAME_UPDATE){
+        PROC_ON_GAME_UPDATE(engine->gamestate);
+    }
 }
 //############################################
 void on_engine_closed(Engine* engine ){
-    printf("on_engine_closed  ..\n") ; 
+    typedef void (*ON_GAME_FINALIZE)(GameState*);
+    //engine->gamestate = (GameState*)engine->gamestate ;
+    PROC_ON_GAME_FINALIZE(engine->gamestate) ;
+    if(engine->gameloader){
+        dlclose(engine->gameloader);
+    }
 }
 //############################################
 void on_engine_swaped(Engine* engine ){
-    printf("on_engine_swaped  ..\n") ; 
+    printInfo("on_engine_swaped  ..\n") ; 
 }
 //############################################
 
